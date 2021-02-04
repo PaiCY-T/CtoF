f94af457af58d897de384ee9ca5d7c747c94e4b7
PaiCY-T <ohsendtome.jww@gmail.com>
2021-02-04 22:29:59 +0800

    update practice1.py

--- practice1.py
+++ practice1.py
@@ -3,8 +3,10 @@ x = rnd.randint(1,100)
 print(x)
 while True:
 	y = int(input('猜數字吧'))
-	if x != y:
-		print('猜錯了')
+	if x > y:
+		print('大一點')
+	elif x < y:
+		print('小一點')
 	else:
 		print('猜對了')
 		break
